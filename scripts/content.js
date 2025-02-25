b9ae101caf5d61330d3908d615f378e11dd51d3b
Rohamunira <mkk88aytax2cayfazt8@gmail.com>
2025-02-25 07:50:07 +0700

    update

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    images = document.createElement("img");
-    images.src = chrome.runtime.getURL("/images/images.png");
-    document.body.innerHTML = ""; // https://raw.githubusercontent.com/Rohamunira/adolf-hitler-stopping-you/refs/heads/main/images/images.png
-    document.body.appendChild(images);
-    document.style = "background-color: #000;";
-});
+images = document.createElement("img");
+images.src = chrome.runtime.getURL("/images/images.png");
+document.body.innerHTML = ""; // https://raw.githubusercontent.com/Rohamunira/adolf-hitler-stopping-you/refs/heads/main/images/images.png
+document.body.appendChild(images);
+document.style = "background-color: #000;";
