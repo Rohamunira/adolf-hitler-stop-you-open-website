d1c648b703657b3e33d3c7b1bdf12a386d859ecd
Rohamunira <mkk88aytax2cayfazt8@gmail.com>
2025-02-25 07:09:23 +0700

    update

--- scripts/content.js
+++ scripts/content.js
@@ -1,2 +1,2 @@
-document.body.innerHTML = "<img src=\"https://raw.githubusercontent.com/Rohamunira/adolf-hitler-stopping-you/refs/heads/main/images/images.png\">";
+document.body.innerHTML = "<img src=\"" + runtime.geturl("images/images.png") +"\">"; // https://raw.githubusercontent.com/Rohamunira/adolf-hitler-stopping-you/refs/heads/main/images/images.png
 document.style = "background-color: #000;";
